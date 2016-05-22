5b5e65e4528505f79f6a19a451be5c599d386f54
Hiroshi Kawasaki <h.kawasaki@studio4096.com>
2016-05-22 21:16:18 +0900

    現行のソースをひとまず動くところまで移行(画像はcloudinaryに)

---
+++ src/requireConfig.js
@@ -0,0 +1,21 @@
+/*globals require*/
+require.config({
+    shim: {
+
+    },
+    paths: {
+        famous: '../lib/famous/src',
+        requirejs: '../lib/requirejs/require',
+        requirepolyfills: '../lib/famous-polyfills/index',
+        almond: '../lib/almond/almond',
+        'famous-flex': '../lib/famous-flex/src',
+        'js-yaml': '../lib/js-yaml/dist/js-yaml',
+        fastclick: '../lib/fastclick/lib/fastclick',
+        'famous-bkimagesurface': '../lib/famous-bkimagesurface',
+        underscore: '../lib/underscore/underscore'
+    },
+    packages: [
+
+    ]
+});
+require(['main']);
